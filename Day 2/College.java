856b4a13fcce1ff97ed83c11e11ea41c04e83ca1
Venu-8008 <venuchepyala008@gmail.com>
2024-12-13 13:54:20 +0530

    Add files via upload

---
+++ Day 2/College.java
@@ -0,0 +1,15 @@
+//program demonstrating instance variable
+
+
+public class College{
+	String CollegeCode;
+	String CollegeName;
+	String Address;
+	Long PhoneNumber;
+	String Affliation="JNTU";
+	void collegeDetails(){
+		System.out.println("CollegeName:"+CollegeName+"\n"+"CollegeCode:"+CollegeCode+"\n"+"Address:"+Address+"\n"+"PhoneNumber:"+PhoneNumber+"\n"+"Affliation:"+Affliation);
+	}
+	
+	
+}
---
+++ Day 2/CollegeDemo.java
@@ -0,0 +1,20 @@
+public class CollegeDemo{
+	public static void main(String [] args){
+		College c1=new College();
+		c1.CollegeCode="123";
+		c1.CollegeName="AVIH";
+		c1.Address="gunthapalli";
+		c1.PhoneNumber=1234l;
+		c1.collegeDetails();
+
+		College c2=new College();
+		c2.CollegeCode="1267";
+		c2.CollegeName="ASRA";
+		c2.Address="gunthapalli";
+		c2.PhoneNumber=19876l;
+		c2.collegeDetails();
+		
+	
+
+	}
+}
